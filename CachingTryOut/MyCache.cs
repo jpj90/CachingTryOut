f3aa8ff072204546000f54f517f57d8c0ddc2a45
James Janse <james.janse@hypoport.com>
2020-07-06 18:44:02 +0200

    Add project files.

---
+++ CachingTryOut/MyCache.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+
+namespace CachingTryOut
+{
+  public class MyCache
+  {
+    private readonly ObjectCache _cache;
+    private const string Key = "my_items";
+    private readonly object _cacheLock = new object();
+
+    public MyCache()
+    {
+      _cache = MemoryCache.Default;
+      CacheItemPolicy policy = GetPolicy();
+      SetInitialItems(policy);
+    }
+
+    public List<Item> GetCachedItems()
+    {
+      lock (_cacheLock)
+      {
+        return _cache.Get(Key) as List<Item>;
+      }
+    }
+
+    public void RefreshCachedItems()
+    {
+      SetUpdatedItems();
+    }
+
+    private void SetInitialItems(CacheItemPolicy policy)
+    {
+      var items = new List<Item>
+      {
+        new Item()
+        {
+          Id = 1, Name = "Item 1"
+        },
+        new Item()
+        {
+          Id = 2, Name = "Item 2"
+        }
+      };
+      lock (_cacheLock)
+      {
+        _cache.Set(Key, items, policy);
+      }
+    }
+
+    private void SetUpdatedItems()
+    {
+      lock (_cacheLock)
+      {
+        _cache.Remove(Key);
+      }
+
+      //Thread.Sleep(2000);
+
+      var items = new List<Item>
+      {
+        new Item()
+        {
+          Id = 1, Name = "Item 1 - Updated"
+        },
+        new Item()
+        {
+          Id = 2, Name = "Item 2 - Updated"
+        },
+        new Item()
+        {
+          Id = 3, Name = "Item 3"
+        },
+        new Item()
+        {
+          Id = 4, Name = "Item 4"
+        }
+      };
+      var policy = GetPolicy();
+      lock (_cacheLock)
+      {
+        _cache.Set(Key, items, policy);
+      }
+    }
+
+    private CacheItemPolicy GetPolicy()
+    {
+      var policy = new CacheItemPolicy();
+      policy.AbsoluteExpiration = DateTimeOffset.Now.AddHours(1.0);
+      policy.RemovedCallback = arguments => ShowCacheClearedMessage();
+      return policy;
+    }
+
+    private void ShowCacheClearedMessage()
+    {
+      Console.WriteLine("Cache has been cleared");
+    }
+  }
+}
---
+++ CachingTryOut/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CachingTryOut
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var cache = new MyCache();
+
+      //ReadAndUpdateCacheSimple(cache);
+
+      for (int i = 0; i < 30; i++)
+      {
+        var taskId = i;
+        Task.Factory.StartNew(() => ReadCacheAndCountItems(cache, taskId));
+        //Console.WriteLine($"started task {taskId}");
+      }
+
+      Random rnd = new Random();
+      int rnNum = rnd.Next(1, 20);
+      Thread.Sleep(1000 * rnNum);
+
+      cache.RefreshCachedItems();
+
+      Console.Read();
+    }
+
+    public static void ReadCacheAndCountItems(MyCache cache, int i)
+    {
+      Random rnd = new Random();
+      int rnNum = rnd.Next(1, 20);
+      Thread.Sleep(200 * rnNum);
+
+      var itemCount = cache.GetCachedItems().Count;
+      Console.WriteLine($"Task {i}: found {itemCount} items in cache after sleeping for {200 * rnNum}");
+    }
+
+    static void ReadAndUpdateCacheSimple(MyCache cache)
+    {
+      var cachedItems = cache.GetCachedItems();
+      foreach (var item in cachedItems)
+      {
+        Console.WriteLine($"found item with id {item.Id } and name {item.Name}");
+      }
+
+      cache.RefreshCachedItems();
+      cachedItems = cache.GetCachedItems();
+
+      foreach (var item in cachedItems)
+      {
+        Console.WriteLine($"found item with id {item.Id } and name {item.Name}");
+      }
+    }
+  }
+}
